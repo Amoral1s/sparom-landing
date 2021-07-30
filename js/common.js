dd2ac5087ef73fcfa2e32a67a7ea2999ad03ded5
Dmitriy Filippov <Amoralis.19@gmail.com>
2021-07-30 20:55:49 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function ($) {
+
+  if(window.width() <= 992) {
+    $('.video').remove();
+  } else {
+    $('.video div').on('click', function() {
+      $(this).fadeOut(200);
+      $('.video iframe').fadeIn(200);
+    });
+  }
+  
+  
+
+  (function() {
+    var start = new Date;
+    start.setHours(0, 0, 0); // 11pm
+  
+    function pad(num) {
+      return ("0" + parseInt(num)).substr(-2);
+    }
+  
+    function tick() {
+      var now = new Date;
+      if (now > start) { // too late, go to tomorrow
+        start.setDate(start.getDate() + 1);
+      }
+      var remain = ((start - now) / 1000);
+      var hh = pad((remain / 60 / 60) % 60);
+      var mm = pad((remain / 60) % 60);
+      var ss = pad(remain % 60);
+
+
+      if (hh >= 23 && hh <= 2 ) {
+        
+        $('.hour').text('00');
+        $('.min').text('00');
+        $('.sec').text('00');
+      } else {
+        $('.hour').text(hh);
+        $('.min').text(mm);
+        $('.sec').text(ss);
+      }
+      
+      setTimeout(tick, 1000);
+    }
+  
+    tick();
+  })();
+
+
+});
